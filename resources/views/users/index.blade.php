89a3a5066c55801459821528111475ff293cbbef
FernandoOnate <ofertar.fernando@gmail.com>
2022-05-10 23:10:29 -0500

    arreglo la vista de los usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,26 +8,28 @@
     <title>Inicio</title>
   </head>
   <body>
-    <h1>Bienvenido!, vea nuestros productos más recientes</h1>
-    @foreach($productos as $item)
-    <div class="row">
+    <div class="container">
+        <h1 class="mt-4">Bienvenido!, vea nuestros productos más recientes</h1>
+        <div class="row">
+            @foreach($productos as $item)
+            <div class="card shadow col-md-3 text-center m-2">
+                <div class="card-header">
+                    <h4 class="title">{{$item->nombre}}</h4>
+                </div>
+                <div class="card-body">
+                    <figure>
+                        <figcaption>{{$item->descripcion}}</figcaption><br>
+                        <img src="{{$item->imagen}}" alt="Imagen del producto" width="200px">
+                        <br><br>
+                        <p>Precio: ${{$item->precio}}</p>
+                        <button class="btn btn-primary btn-sm">Comprar</button>
+                    </figure>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 
-    </div>
-<div class="card shadow ">
-    <div class="card-header">
-        <h4 class="title">{{$item->nombre}}</h4>
-    </div>
-    <div class="card-body">
-        <figure>
-            <figcaption>Descripción: {{$item->descripcion}}</figcaption>
-            <img src="{{$item->imagen}}" alt="Imagen del producto" width="200px">
-            <p>Precio: ${{$item->precio}}</p>
-            <button class="btn btn-primary btn-sm">Comprar</button>
-        </figure>
-    </div>
-</div>
-<p></p>
-@endforeach
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
